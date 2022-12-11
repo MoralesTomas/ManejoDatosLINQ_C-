8e893b5d4748e19764f7d6601dd364a7436837a9
AlejoMora991014 <alejandroester1910@gmail.com>
2022-12-11 12:41:35 -0600

    creando clase para importar los datos

--- AplicaconConsola_LINQ/Clases/LinqQueries.cs
+++ AplicaconConsola_LINQ/Clases/LinqQueries.cs
@@ -1,7 +1,41 @@
 public class LinqQueries
 {
+    private List<Book> librosCollection = new List<Book>();
+
     public LinqQueries()
     {
-        
+
+
+        using(StreamReader reader = new StreamReader("Libros/Libros.json"))
+        {
+            
+            //vamos a guardar el contenido dentro de un string utilizando el metodo ReadToEnd 
+            //para que lea el archivo de principio a fin
+
+            string json = reader.ReadToEnd();
+
+            //ahora vamos a tomar el texto y a convertirlo en una conleccion
+            this.librosCollection = System.Text.Json.JsonSerializer.Deserialize<List<Book>>(json, new System.Text.Json.JsonSerializerOptions() {PropertyNameCaseInsensitive = true});
+        }
+    }
+
+    public IEnumerable<Book> TodaLaColeccion()
+    {
+        return librosCollection;
     }
 }
+
+
+/*
+Donde:
+
+    >la opcion .Text.Json.JsonSerializer lo que hace es convertir el texto en un json que podemos manejar
+
+    >luego lo convertimos en un tipo de dato definido por nosotros dependiendo lo que estemos reciviendo el json. En este caso queremos
+    un listado de libros (es lo que viene en el json) "List<libro>" y le indicamos con que string debe trabajar en este caso es con el string
+    llamado "json".
+
+    >agregamos una propiedad mas para que las propiedades de cada objeto json se enlacen con las propiedades de la clase que definimos es decir 
+    que ignore si vienen mayusculas o minusculas en las propiedades y que se enlace en sus distintas propiedades.
+
+*/
--- AplicaconConsola_LINQ/Program.cs
+++ AplicaconConsola_LINQ/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿LinqQueries queries = new LinqQueries();
+
+ImprimirValores(queries.TodaLaColeccion());
+
+void ImprimirValores(IEnumerable<Book> listadelibros)
+{
+    //imprimir titulos
+    Console.WriteLine("{0,-60} {1, 15} {2, 15}\n", "Titulo", "N. Paginas", "Fecha publicacion");
+    
+    //imprimir cada elemento segun la coleccion que nos manden
+    foreach(var item in listadelibros)
+    {
+        Console.WriteLine("{0,-60} {1, 15} {2, 15}", item.Title, item.PageCount, item.PublishedDate.ToShortDateString());
+    }
+}
